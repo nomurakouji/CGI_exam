ca824f3ee9356016eef5dec1eb57523181e20f02
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-11-19 14:24:11 +0900

    課題の提出

---
+++ exam.rb
@@ -0,0 +1,13 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '2000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/exam', WEBrick::HTTPServlet::ERBHandler, 'exam.html.erb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/goya2.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya2.rb')
+server.start
